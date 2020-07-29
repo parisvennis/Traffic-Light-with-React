d7cfc8d93943cca8d8a73a17a42b7664e11c4baa
parisvennis <parisvennis@gmail.com>
2020-07-29 20:14:50 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,32 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	var color = "";
+	var redOn = "";
+	if (color == "red") redOn = "active";
+	var yellowOn = "";
+	if (color == "yellow") yellowOn = "active";
+	var greenOn = "";
+	if (color == "green") greenOn = "active";
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="bar">bar</div>
+			<div className="light text-center mt-5">
+				<div onClick={() => } className={"r-light" + redOn}>
+					red light
+				</div>
+				<div
+					onClick={() => }
+					className={"y-light" + yellowOn}>
+					yellow light
+				</div>
+				<div
+					onClick={() => }
+					className={"g-light" + greenOn}>
+					green light
+				</div>
+			</div>
+		</>
 	);
 }
